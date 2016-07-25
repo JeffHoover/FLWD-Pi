4fd52039274d1fb11a2ded57bd7565cb10993088
Jeff Hoover <jhoover@jhoover.com>
2016-07-25 07:58:27 -0400

    stripping down to bare bones

--- flwd.py
+++ flwd.py
@@ -8,7 +8,6 @@ import signal
 import RPi.GPIO as GPIO
 from Adafruit_LED_Backpack import AlphaNum4
 
-# My python files:
 import words
 
 def setup_GPIO():
@@ -27,48 +26,11 @@ def signal_handler(signal, frame):
         os.system('echo 1 | sudo tee /sys/class/leds/led1/brightness')
         sys.exit(0)
 
-
-def update_offense_level_from_switch(word, offense_level):
-    if GPIO.input(SWITCH_GPIO_PIN) == GPIO.LOW:
-        if offense_level <= 4:    
-            offense_level += 1
-        word = "   " + str(offense_level)
-    return word
-
-
-def get_offensive_word():
-    word = "okok"
-    while not word.endswith("*"):
-        word = random.choice(words.words)
-    return word[0:4]
-
-
 def get_clean_word():
     word = "*"
     while word.endswith("*"):
         word = random.choice(words.words)
     return word
-
-
-def get_random_word():
-    word = random.choice(words.words)
-    word.endswith("*")
-    if word.endswith("*"):
-        return word[0:4]
-    return word
-
-
-def get_word_based_on_offense_level(offense_level):
-    function_chooser = {
-        0: get_clean_word(),    # totally clean
-        1: get_clean_word(),    # TODO - biased toward clean
-        2: get_random_word(),   # equally random
-        3: get_random_word(),   # TODO - biased toward dirty
-        4: get_offensive_word() # only dirty
-    }
-    get_word = function_chooser.get(offense_level, lambda:get_clean_word())
-    return get_word
-
 
 def display_word(word):
     display.print_str(word)
@@ -76,29 +38,15 @@ def display_word(word):
     print(word)
     time.sleep(0.8 + random.random())
 
-
 def display_startup_message():
     for start_word in words.startup_message:
         display_word(start_word)
-        if GPIO.input(SWITCH_GPIO_PIN) == GPIO.LOW:
-            return
         time.sleep(0.8)
 
-# app starts here
-#---------------------------
-
-#TODO - put back to zero
-offense_level = 2
-cleaner_button = 0
-dirtier_button = 0 
 word = "init"
-SWITCH_GPIO_PIN = 12
 
 #initialize the ctrl-c callback
 signal.signal(signal.SIGINT, signal_handler)
-
-# assign stderr to capture annoying (but ignorable) pull-up resistor errors:
-sys.stderr = open('stderr.txt', 'w')
 
 # Create display instance on default I2C address (0x70) and bus number.
 display = AlphaNum4.AlphaNum4()
@@ -118,15 +66,5 @@ if len(sys.argv) == 1:
 # Main Loop:
 #------------
 while True:
-    word = get_word_based_on_offense_level(offense_level)
-    word = update_offense_level_from_switch(word, offense_level)
-    if word.startswith(" "):
-        new_offense_level = word[3:]
-        offense_level = int(new_offense_level)
-        if offense_level > 4:
-            offense_level = 0
-            word = "   0"
-
-    display_word(word)
-
+    display_word(get_clean_word())
 
